88609dfbdf05fcc99acf412da39f8caafc344e4c
Gabriel "Kinash <gabriel@kinash.xyz>
2022-05-16 19:46:36 -0300

    adding more complexity

--- contracts/LeaderZombies.sol
+++ contracts/LeaderZombies.sol
@@ -3,19 +3,23 @@ pragma solidity ^0.8.9;
 pragma abicoder v2;
 
 import "erc721a/contracts/ERC721A.sol";
+import "erc721a/contracts/extensions/ERC721AQueryable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract LeaderZombies is ERC721A, Ownable, ReentrancyGuard {
+contract LeaderZombies is ERC721A, ERC721AQueryable, Ownable, ReentrancyGuard {
     using Strings for uint256;
     
     uint256 public immutable collectionSize;
 
+    address public immutable payableAddress;
+
     string public defaultBaseURI;
 
-    constructor(uint256 _collectionSize, string memory _defaultBaseURI) ERC721A("Fantom Zombies Leader", "LEADERZOMBIES") {
+    constructor(uint256 _collectionSize, address _payableAddress, string memory _defaultBaseURI) ERC721A("Fantom Zombies Leader", "LEADERZOMBIES") {
         collectionSize = _collectionSize;
+        payableAddress = _payableAddress;
         defaultBaseURI = _defaultBaseURI;
     }
 
@@ -25,8 +29,14 @@ contract LeaderZombies is ERC721A, Ownable, ReentrancyGuard {
     }
 
     function withdraw() external onlyOwner nonReentrant {
-        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        (bool success, ) = payableAddress.call{
+            value: address(this).balance
+        }("");
         require(success, "Withdraw failed.");
+    }
+
+    function setBaseURI(string calldata newURI) external onlyOwner {
+        defaultBaseURI = newURI;
     }
 
     function tokenURI(uint256 tokenId) public view override(ERC721A) returns (string memory) {
@@ -35,5 +45,9 @@ contract LeaderZombies is ERC721A, Ownable, ReentrancyGuard {
 
     function _baseURI() internal view override(ERC721A) returns (string memory) {
         return defaultBaseURI;
+    }
+
+    function _startTokenId() internal pure override(ERC721A) returns (uint256) {
+        return 1;
     }
 }
