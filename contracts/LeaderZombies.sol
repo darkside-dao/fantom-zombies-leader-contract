697babe3c399f2a3ce40ca4e9c637d1b2146ccd4
Gabriel "Kinash <gabriel@kinash.xyz>
2022-05-31 18:46:00 -0300

    remove unecessary imports & make token uri tests

--- contracts/LeaderZombies.sol
+++ contracts/LeaderZombies.sol
@@ -6,11 +6,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract LeaderZombies is ERC721URIStorage, ERC721Enumerable, ERC721Burnable, ERC721Pausable, ReentrancyGuard, Ownable {
+contract LeaderZombies is ERC721URIStorage, ERC721Enumerable, ERC721Burnable, ERC721Pausable, Ownable {
     using Counters for Counters.Counter;
 
     uint256 immutable public collectionSize;
---
+++ test/leader-test.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  let leaderZombies, accounts;
+  beforeEach(async function () {
+    const LeaderZombies = await ethers.getContractFactory("LeaderZombies");
+    leaderZombies = await LeaderZombies.deploy(50);
+    await leaderZombies.deployed();
+    accounts = await ethers.getSigners();
+  });
+
+  it("Check token uri", async function () {
+    const tx = await leaderZombies.give(accounts[0].address, 2);
+    await tx.wait();
+
+    const tx1 = await leaderZombies.setTokenURI(1, "kinash");
+    await tx1.wait();
+
+    expect(await leaderZombies.tokenURI(1)).to.be.equal("kinash");
+    expect(await leaderZombies.tokenURI(2)).to.be.equal("");
+  });
+});
